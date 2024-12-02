93d359b030fbb336eb4447c18e43481408eeac91
T0n0T <823478402@qq.com>
2024-12-02 07:28:27 +0000

    change vite.config from ts to js

--- web/vite.config.js
+++ web/vite.config.js
@@ -5,8 +5,7 @@ import vue from '@vitejs/plugin-vue'
 export default defineConfig({
   plugins: [vue()],
   server: {
-    host: '0.0.0.0', // 这个用于启动
-    port: 8080, // 指定启动端口
-    open: false, //启动后是否自动打开浏览器
+    port: 8080, 
+    open: false,
   },
 })
