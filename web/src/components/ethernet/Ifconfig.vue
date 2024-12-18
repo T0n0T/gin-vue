218f3c56d9506b330382451146f51f47bd9aed5f
T0n0T <823478402@qq.com>
2024-12-19 00:13:36 +0800

    add eth vue

--- web/src/components/ethernet/Ifconfig.vue
+++ web/src/components/ethernet/Ifconfig.vue
@@ -5,7 +5,7 @@
 
         <!-- Dialog for NIC configuration -->
         <el-dialog title="网卡配置" v-model="dialogVisible" width="50%" @close="resetForm">
-            <el-form :model="form" ref="configForm" label-width="100px" label-position="right">
+            <el-form :model="form" ref="configForm" :rules="rules" label-width="100px" label-position="right">
                 <!-- DHCP开关 -->
                 <el-form-item label="DHCP">
                     <el-switch v-model="form.dhcp" active-text="开" inactive-text="关"></el-switch>
@@ -25,37 +25,94 @@
                     <el-input v-model="form.dns" placeholder="请输入DNS服务器"></el-input>
                 </el-form-item>
             </el-form>
+            <template #footer>
+                <span class="dialog-footer">
+                    <el-button @click="dialogVisible = false">取消</el-button>
+                    <el-button type="primary" @click="saveConfig">保存</el-button>
+                </span>
+            </template>
         </el-dialog>
     </div>
 </template>
 
-<script>
-export default {
-    name: 'Ifconfig',
-    data() {
-        return {
-            dialogVisible: false,
-            form: {
-                dhcp: false, // 初始化为关闭状态
-                ip: '',
-                subnetMask: '',
-                gateway: '',
-                dns: ''
-            }
-        };
-    },
-    methods: {
-        resetForm() {
-            this.form = {
-                dhcp: false,
-                ip: '',
-                subnetMask: '',
-                gateway: '',
-                dns: ''
-            };
+<script setup>
+import { ref } from 'vue'
+import { ElMessage } from 'element-plus'
+
+const dialogVisible = ref(false);
+const form = ref({
+    dhcp: false,
+    ip: '',
+    subnetMask: '',
+    gateway: '',
+    dns: ''
+});
+const rules = ref({
+    ip: [
+        { required: true, message: '请输入IP地址', trigger: 'blur' },
+        {
+            pattern: /^(\d{1,3}\.){3}\d{1,3}$/,
+            message: '请输入正确的IP地址',
+            trigger: 'blur'
         }
-    }
-}
+    ],
+    subnetMask: [
+        { required: true, message: '请输入子网掩码', trigger: 'blur' },
+        {
+            pattern: /^(\d{1,3}\.){3}\d{1,3}$/,
+            message: '请输入正确的子网掩码',
+            trigger: 'blur'
+        }
+    ],
+    gateway: [
+        { required: true, message: '请输入默认网关', trigger: 'blur' },
+        {
+            pattern: /^(\d{1,3}\.){3}\d{1,3}$/,
+            message: '请输入正确的默认网关',
+            trigger: 'blur'
+        }
+    ],
+    dns: [
+         { required: false, message: '请输入DNS服务器', trigger: 'blur' },
+        {
+            pattern: /^(\d{1,3}\.){3}\d{1,3}$/,
+            message: '请输入正确的DNS服务器',
+            trigger: 'blur'
+        }
+    ]
+});
+
+const configForm = ref(null);
+
+const openDialog = () => {
+    dialogVisible.value = true
+};
+
+const resetForm = () => {
+    form.value = {
+        dhcp: false,
+        ip: '',
+        subnetMask: '',
+        gateway: '',
+        dns: ''
+    };
+};
+
+const saveConfig = () => {
+    configForm.value.validate((valid) => {
+        if (valid) {
+            console.log('Form submitted:', form.value)
+            dialogVisible.value = false;
+            ElMessage({
+                message: '配置已保存',
+                type: 'success'
+            });
+        } else {
+            console.log('error submit!!')
+            return false;
+        }
+    });
+};
 </script>
 
 <style scoped>
--- web/src/views/ethernet.vue
+++ web/src/views/ethernet.vue
@@ -1,12 +1,47 @@
 <template>
     <div class="eth-container">
         <el-main>
+            <el-dialog title="新增连接" v-model="newConnDialogVisible" width="50%">
+
+                <template #footer>
+                    <span class="dialog-footer">
+                        <el-button @click="newConnDialogVisible = false">取消</el-button>
+                        <el-button type="primary" @click="newConn">保存</el-button>
+                    </span>
+                </template>
+            </el-dialog>
             <Ifconfig />
         </el-main>
-        <el-button class="add-button" circle type="primary" :icon="Plus" @click="openConnCreateDialog" />
+        <el-button class="add-button" circle type="primary" :icon="Plus" @click="openNewConnDialog" />
         <el-button class="del-button" circle type="primary" :icon="DeleteFilled" @click="clearConns()" />
     </div>
 </template>
+
+<script setup>
+import { Plus, DeleteFilled } from '@element-plus/icons-vue'
+import { ref } from 'vue'
+import { ElMessage } from 'element-plus'
+import Ifconfig from '@/components/ethernet/Ifconfig.vue'
+
+const newConnDialogVisible = ref(false);
+
+const openNewConnDialog = () => {
+    newConnDialogVisible.value = true
+    // 实现扫描对话框逻辑
+}
+
+const newConn = () => {
+    newConnDialogVisible.value = false;
+    ElMessage({
+        message: '连接创建成功',
+        type: 'success'
+    })
+}
+
+const clearConns = () => {
+    // 实现清除设备逻辑
+}
+</script>
 
 <style scoped>
 .eth-container {
@@ -39,16 +74,3 @@
     padding: 20px;
 }
 </style>
-
-<script setup>
-import { Plus, DeleteFilled } from '@element-plus/icons-vue'
-import Ifconfig from '@/components/ethernet/Ifconfig.vue'
-
-const openConnCreateDialog = () => {
-    // 实现扫描对话框逻辑
-}
-
-const clearConns = () => {
-    // 实现清除设备逻辑
-}
-</script>
